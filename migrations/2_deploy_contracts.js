485567a6e1b237fac47f0f0c4e3f1c7d88f67a27
Seif Khedija <saifkhdija@gmail.com>
2022-05-05 11:22:26 +0100

    added uniswap contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,7 +9,16 @@ const UniswapV2Router02 = artifacts.require("UniswapV2Router02");
 const WETH = artifacts.require("WETH");
 module.exports = async function(deployer, network, accounts) {
   const [admin, _] = accounts
-  await deployer.deploy(WETH);
+
+  await deployer.deploy(PortfolioManagement);
+  const portfoliomanagement = await PortfolioManagement.deployed();
+
+  await deployer.deploy(PortfolioToken);
+  const tortfoliotoken = await PortfolioToken.deployed();
+
+  await deployer.deploy(PortfolioNFT);
+  const portfolionFT = await PortfolioNFT.deployed();
+  /*await deployer.deploy(WETH);
   const weth = await WETH.deployed();
   const tokenA = await ERC20Mock.new('token A', 'TKA', web3.utils.toWei(String(1000),'ether'));
   const tokenB = await ERC20Mock.new('token B', 'TKB', web3.utils.toWei(String(1000),'ether'));
@@ -17,8 +26,10 @@ module.exports = async function(deployer, network, accounts) {
   const factory = await UniswapV2Factory.deployed();
   await factory.createPair(weth.address,tokenA.address);
   await factory.createPair(weth.address,tokenB.address);
+  await factory.createPair(tokenA.address,tokenB.address);
   await deployer.deploy(UniswapV2Router02,factory.address,weth.address);
   const router = await UniswapV2Router02.deployed();
+  */
 
 
 }
--- test/swap_tokens.js
+++ test/swap_tokens.js
@@ -5,3 +5,58 @@ const UniswapV2Factory = artifacts.require('UniswapV2Factory')
 const PortfolioManagement = artifacts.require('PortfolioManagement')
 const PortfolioToken = artifacts.require('PortfolioToken')
 const PortfolioNFT = artifacts.require('PortfolioNFT')
+const ERC20Mock = artifacts.require('ERC20Mock')
+const { ethers } = require("ethers");
+function tokens(n) {
+    return web3.utils.toWei(n, 'ether');
+}
+contract('UniswapV2Factory', ([owner, provider]) => {
+    let factory,weth,pair,root,tokenA,tokenB,pairAdress,pairSwap,router,created_at,amountIn
+    before(async () => {
+        weth = await WETH.new()
+        pair = await UniswapV2Pair.new()  
+        factory = await UniswapV2Factory.new(owner)
+        router = await UniswapV2Router02.new(factory.address,weth.address)
+        // creating 2 tokens to create a pool
+        tokenA = await ERC20Mock.new('token A', 'TKA', String(1000))
+        tokenB = await ERC20Mock.new('token B', 'TKB', String(1000))
+        // trasfer amount to both tokens
+        await tokenA.transfer(provider, 100, { from: owner })
+        await tokenB.transfer(provider, 100, { from: owner })
+        await tokenA.approve( router.address, 100, { from: provider } )
+        await tokenB.approve( router.address, 100, { from: provider } )
+        // Create pool using tokenA and tokenB pair
+        await factory.createPair(tokenA.address,tokenB.address)
+        created_at = Math.floor(Date.now() / 1000)
+    //    await router.addLiquidity(tokenA.address, tokenB.address,50,50,0,0,provider,created_at)
+        // swap amount of tokenA into TokenB
+      /*  await router.swapExactTokensForTokens(
+          tokens('30'),
+          0,
+          [tokenA.address, tokenB.address],
+          provider,
+          created_at,
+          { from: provider})*/
+      })
+      describe('Adding pair', async () => {
+        
+          it('Provider has enaugh tokenB', async () => {
+            let balanceB = await tokenB.balanceOf(provider)
+            assert.equal(balanceB.toString(), 100)
+          })
+          it('pair added to the list', async () => {
+              let length = await factory.allPairsLength()
+            assert.equal(length, 1,"there is no pair ")
+          })
+          it('Pool tokenA-TokenB created', async () => {
+          let addr = await factory.getPair(tokenA.address, tokenB.address)
+            assert.notEqual(addr,'0x0000000000000000000000000000000000000000',"tokenA tokenB exist")
+          })
+          it('swapTokens', async () => {
+            // balance of provider after swap
+             let ba = await tokenA.balanceOf(provider)
+              assert.equal(ba,70,"balance is wrong")
+            })
+      })
+  })
+  
